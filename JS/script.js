fd28e096490aed372cae59f9d3580285eec74dd6
P-Lautaro <parritalauti@gmail.com>
2022-11-01 17:09:01 -0300

    agregando fuction, var y creando elemento

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
 var formu = document.getElementById("formu");
+
+
+function addElemento(texto){
+    var capa = document.getElementById("formu");
+    var h1 = document.createElement("h1");
+    h1.innerHTML = texto;
+    formu.appendChild(h1);
+}
